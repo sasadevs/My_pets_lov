146a6e99355bcc2d953d410e94ad62a18acf1573
sasadevs <sarinha.chate@gmai.com>
2024-05-02 22:11:24 -0300

    Conexão entre xml e java, entre login, senha e botão de sair e entrar

--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -3,6 +3,10 @@ package br.com.etecia.myapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
+    EditText edtEmail, edtSenha;
+    Button btnSair, btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,56 @@ public class SplashActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtSenha = findViewById(R.id.edtSenha);
+        btnEntrar = findViewById(R.id.btnEntrar);
+        btnSair = findViewById(R.id.btnSair);
+
+
+
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+
+
+        });
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, senha;
+
+                email = edtEmail.getText().toString();
+                senha = edtSenha.getText().toString();
+
+                if (email.equals("etecia") && senha.equals("etecia")){
+                    Intent intent = new Intent(getApplicationContext(),
+                            MenuprincipalActivity.class);
+                    startActivity(Intent);
+
+                    finish();
+
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "Usuário ou senha inválidos!!!", Toast.LENGTH_SHORT).show();
+
+
+                Toast.makeText(getApplicationContext(), "Acessando o Sistemas!!!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
 
         //criar método thread para abrir janela
         new Handler().postDelayed(new Runnable() {
